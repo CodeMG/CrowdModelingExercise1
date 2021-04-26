529f68f4bafb1a70af59735f5501fd4485abc871
gjke <sergii.poluektov@gmail.com>
2021-04-26 09:14:23 +0200

    refactor: move get_neighbours to parent class

--- lib/update_schemes.py
+++ lib/update_schemes.py
@@ -23,11 +23,11 @@ class UpdateScheme(ABC):
 
     def move_pedestrian(self, p: Coordinates, costs: np.ndarray) -> Coordinates:
         # all the neighbours of this pedestrian and the costs of moving there
-        options = [p, (p[0] - 1, p[1]), (p[0] + 1, p[1]),
-                   (p[0], p[1] - 1), (p[0], p[1] + 1), (p[0] - 1, p[1] - 1), (p[0] + 1, p[1] + 1), (p[0] - 1, p[1] + 1), (p[0] + 1, p[1] - 1)]
+        neighbours = UpdateScheme.get_neighbours(
+            p, costs.shape[0], costs.shape[1], diagonal=True) + [p]
+
         options_with_cost = [
-            (option, costs[option]) for option in options
-            if 0 <= option[0] <= costs.shape[0]-1 and 0 <= option[1] <= costs.shape[1]-1
+            (option, costs[option]) for option in neighbours
         ]
         return min(options_with_cost, key=lambda pair: pair[1])[0]
 
@@ -43,6 +43,26 @@ class UpdateScheme(ABC):
                 )
             )
         )
+
+    @classmethod
+    def get_neighbours(cls, c: Coordinates, rows: int, columns: int, diagonal: bool = False) -> List[Coordinates]:
+        options = [
+            (c[0] - 1, c[1]),
+            (c[0] + 1, c[1]),
+            (c[0], c[1] - 1),
+            (c[0], c[1] + 1)
+        ]
+        if diagonal:
+            options += [
+                (c[0] - 1, c[1] - 1),
+                (c[0] + 1, c[1] + 1),
+                (c[0] - 1, c[1] + 1),
+                (c[0] + 1, c[1] - 1)
+            ]
+        return [
+            option for option in options
+            if 0 <= option[0] < rows and 0 <= option[1] < columns
+        ]
 
 
 class RandomUpdateScheme(UpdateScheme):
@@ -82,19 +102,6 @@ class DijkstraUpdateScheme(UpdateScheme):
 
         recursively_populate_neighbours(grid.target, 0)
         return costs
-
-    @classmethod
-    def get_neighbours(cls, c: Coordinates, rows: int, columns: int):
-        options = [
-            (c[0] - 1, c[1]),
-            (c[0] + 1, c[1]),
-            (c[0], c[1] - 1),
-            (c[0], c[1] + 1)
-        ]
-        return [
-            nc for nc in options
-            if 0 <= nc[0] < rows and 0 <= nc[1] < columns
-        ]
 
 
 class EuclideanInteractiveUpdateScheme(UpdateScheme):
